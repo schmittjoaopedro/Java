6ff6089afa19f5b20da87701c71c8a1ed3a52d31
João Pedro Schmitt <schmittjoaopedro@gmail.com>
2013-11-05 18:04:42 -0200

    Add Serial Session and Serial Exception

---
+++ Release-Java-Serial/src/org/schmitt/framework/serial/communicate/SerialPortException.java
@@ -0,0 +1,18 @@
+package org.schmitt.framework.serial.communicate;
+
+public class SerialPortException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public SerialPortException(String message) {  
+        super(message);  
+    }  
+	
+	public SerialPortException(Exception ex) {  
+        super(ex);  
+    }
+	
+}
