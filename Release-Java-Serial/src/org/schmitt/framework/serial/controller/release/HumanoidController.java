c17e97a749c3568c7cb0dac01313eb305227ee5d
João Pedro Schmitt <schmittjoaopedro@gmail.com>
2013-11-07 15:15:15 -0200

    Altered Size Screen

--- Release-Java-Serial/src/org/schmitt/framework/serial/controller/release/HumanoidController.java
+++ Release-Java-Serial/src/org/schmitt/framework/serial/controller/release/HumanoidController.java
@@ -17,7 +17,7 @@ public class HumanoidController {
 		
 		frame.add(s, BorderLayout.EAST);
 		frame.add(s2,BorderLayout.WEST);
-		frame.setSize(800, 600);
+		frame.setSize(900, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		
